5dbb4514c17234eb2ea6edea53f0b019903d30f4
Aliosha Padovani <aliosha.padovani@gmail.com>
2022-08-27 18:04:02 +0200

    testing new meta tags

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -138,5 +138,7 @@ export const head: HeadConfig[] = [
   ["meta", { name: "application-name", content: "classBot_" }],
   ["meta", { name: "apple-mobile-web-app-title", content: "classBot_" }],
   ["meta", { name: "theme-color", content: "#3eaf7c" }],
-  ["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0, user-scalable=yes"}]
+  ["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=yes"}],
+  ["meta", { name: "mobile-web-app-capable", content: "yes" }],
+  ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
 ];
