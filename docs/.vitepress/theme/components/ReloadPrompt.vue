27a88eadc078698bf6109e6a22963873efb41cea
Aliosha Padovani <aliosha.padovani@gmail.com>
2023-10-22 22:22:29 +0200

    Use CSS variables
    Use CSS variables for border color and background color in ReloadPrompt.vue component
    
    The commit changes the border color and background color of the ReloadPrompt.vue component to use CSS variables instead of hard-coded values. This allows for easier customization and consistency throughout the application.

--- docs/.vitepress/theme/components/ReloadPrompt.vue
+++ docs/.vitepress/theme/components/ReloadPrompt.vue
@@ -70,18 +70,17 @@ onBeforeMount(async () => {
         bottom: 0;
         margin: 16px;
         padding: 12px;
-        border: 1px solid #8885;
+        border: 1px solid var(--vp-c-bg-soft);
         border-radius: 4px;
         z-index: 100;
         text-align: left;
-        box-shadow: 3px 4px 5px 0 #8885;
-        background-color: white;
+        background-color: var(--vp-c-bg-soft);
     }
     .pwa-toast #pwa-message {
         margin-bottom: 8px;
     }
     .pwa-toast button {
-        border: 1px solid #8885;
+        border: 1px solid var(--vp-c-bg-soft);
         outline: none;
         margin-right: 5px;
         border-radius: 2px;
