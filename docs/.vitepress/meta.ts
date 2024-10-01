4ddbaf006f4debbdb4e6c923bc51c3c47a015ae0
Aliosha Padovani <aliosha.padovani@gmail.com>
2024-10-01 17:08:58 +0200

    Update site url

--- docs/.vitepress/meta.ts
+++ docs/.vitepress/meta.ts
@@ -1,7 +1,7 @@
 /** basic site info */
 export const siteTitle = "classBot_";
-export const site = "https://physique-chimie-org.github.io";
-export const logo = "https://physique-chimie-org.github.io/logo.svg";
+export const site = "https://classbot.fr";
+export const logo = "https://classbot.fr/logo.svg";
 export const keywords = "classBot_, physique, chimie, lycée, cours";
 export const siteDescription =
     "Site de cours de physique-chimie, pour les lycéens. Accessible partout, même hors connexion.";
